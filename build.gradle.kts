912f5ea7dac9c047bbf0f34fe7b66c389a0d81bc
Ilkka Poutanen <ilkka@ilkka.dev>
2023-06-25 20:05:28 +0300

    Add actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories { mavenCentral() }
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
