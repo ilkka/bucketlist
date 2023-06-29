072bb504541b48739b12c65b16c801b4d78add4e
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-06-29 09:21:27 +0300

    A list API or something

--- src/main/kotlin/dev/ilkka/bucketlist/HelloController.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/HelloController.kt
@@ -1,13 +1,24 @@
 package dev.ilkka.bucketlist
 
+import dev.ilkka.bucketlist.domain.BucketListItem
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
-    
+
     @GetMapping("/")
     fun index(): String {
         return "Moro!"
+    }
+
+    // Return a list of bucket list items
+    @GetMapping("/bucketlist")
+    fun bucketlist(): List<BucketListItem> {
+        return listOf(
+                BucketListItem("Go to space", "Yes really go to space", setOf()),
+                BucketListItem("Learn to fly an airplane", "Wouldn't it be cool", setOf()),
+                BucketListItem("Learn to play an instrument", "Guitar or piano?", setOf())
+        )
     }
 }
