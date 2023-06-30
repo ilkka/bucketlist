150b12d85c507ffd93c627ae84fbdde696287c3a
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-06-30 13:18:44 +0300

    More API testing

--- src/test/kotlin/dev/ilkka/bucketlist/BucketListControllerTest.kt
+++ src/test/kotlin/dev/ilkka/bucketlist/BucketListControllerTest.kt
@@ -1,5 +1,7 @@
 package dev.ilkka.bucketlist
 
+import dev.ilkka.bucketlist.domain.BucketListItem
+import dev.ilkka.bucketlist.domain.Category
 import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,9 +18,58 @@ public class BucketListControllerTest {
     @Autowired private lateinit var mvc: MockMvc
 
     @Test
-    fun getBucketlist() {
+    fun testAddToBucketList() {
+        // Given a bucket list item
+        val item = BucketListItem("Test item", "Test description")
+
+        // When the item is saved through the API
+        mvc.perform(
+                        MockMvcRequestBuilders.post("/bucketlist")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(
+                                        "{\"title\": \"${item.title}\", \"description\": \"${item.description}\"}"
+                                )
+                )
+                .andExpect(status().isOk())
+
+        // Then the item should be returned by the API
         mvc.perform(MockMvcRequestBuilders.get("/bucketlist").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[]"))
+                .andExpect(
+                        content()
+                                .json(
+                                        "[{\"id\":1,\"title\":\"Test item\",\"description\":\"Test description\",\"categories\":[]}]"
+                                )
+                )
+    }
+
+    @Test
+    fun testAddWithCategory() {
+        // Given a category
+        val category = Category("Test category")
+        // and a bucket list item in that category
+        val item = BucketListItem("Test item", "Test description", setOf(category))
+
+        // When the category is saved through the API and the result is saved to a variable
+        val result =
+                mvc.perform(
+                                MockMvcRequestBuilders.post("/category")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content("{\"title\": \"${category.title}\"}")
+                        )
+                        .andExpect(status().isOk())
+                        .andReturn()
+        val json = result.response.contentAsString
+        val categoryId = json.substringAfter("id\":").substringBefore(",").toLong()
+
+        // When the item is saved through the API
+        mvc.perform(
+                        MockMvcRequestBuilders.post("/bucketlist")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(
+                                        "{\"title\": \"${item.title}\", \"description\": \"${item.description}\", \"categories\": [$categoryId}]}"
+                                )
+                )
+                .andExpect(status().isOk())
     }
 }
