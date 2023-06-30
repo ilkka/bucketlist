2625f3aa613db1dd32be354d505830fd0783e5e2
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-06-30 13:17:34 +0300

    Move controller to right place

--- src/main/kotlin/dev/ilkka/bucketlist/BucketListController.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/BucketListController.kt
@@ -4,6 +4,8 @@ import dev.ilkka.bucketlist.domain.BucketListItem
 import dev.ilkka.bucketlist.domain.BucketListItemRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,7 +14,13 @@ class BucketListController {
 
     // Return a list of bucket list items
     @GetMapping("/bucketlist")
-    fun bucketlist(): List<BucketListItem> {
+    fun getBucketList(): List<BucketListItem> {
         return bucketListItemRepository.findAll().toCollection(destination = ArrayList())
+    }
+
+    // Add a new bucket list item
+    @PostMapping("/bucketlist")
+    fun addBucketListItem(@RequestBody item: BucketListItem): BucketListItem {
+        return bucketListItemRepository.save(item)
     }
 }
