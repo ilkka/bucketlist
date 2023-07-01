d4f301e610450171cf24b3c1e3c7d70957a04331
Ilkka Poutanen <ilkka@ilkka.dev>
2023-07-01 13:49:07 +0300

    chore: Fix the damn project key (#13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories { mavenCentral() }
 
 sonarqube {
     properties {
-        property("sonar.projectKey", "ilkka_buckelist")
+        property("sonar.projectKey", "ilkka_bucketlist")
         property("sonar.organization", "ilkka-dev")
         property("sonar.host.url", "https://sonarcloud.io")
     }
