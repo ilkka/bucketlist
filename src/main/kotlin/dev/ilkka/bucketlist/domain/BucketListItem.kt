15fbea1cbd8bc3bb9b06e057bea5e87f6eaf91cf
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-07-04 14:46:02 +0300

    Make entity classes work with default psql identity sequence names

--- src/main/kotlin/dev/ilkka/bucketlist/domain/BucketListItem.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/domain/BucketListItem.kt
@@ -3,15 +3,22 @@ package dev.ilkka.bucketlist.domain
 import jakarta.persistence.*
 
 @Entity
+@Table(name = "bucketlistitem")
 class BucketListItem(
         val title: String,
         val description: String,
         @ManyToMany
         @JoinTable(
-                name = "bucketlistitem_category",
+                name = "bucketlistitem_categories",
                 joinColumns = [JoinColumn(name = "bucketlistitem_id")],
                 inverseJoinColumns = [JoinColumn(name = "category_id")]
         )
         val categories: Set<Category>? = null,
-        @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bucketlistitem_generator")
+        @SequenceGenerator(
+                name = "bucketlistitem_generator",
+                sequenceName = "bucketlistitem_id_seq"
+        )
+        val id: Long? = null,
 )
--- src/main/kotlin/dev/ilkka/bucketlist/domain/Category.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/domain/Category.kt
@@ -3,11 +3,15 @@ package dev.ilkka.bucketlist.domain
 import jakarta.persistence.*
 
 @Entity
+@Table(name = "category")
 class Category(
         val title: String,
         @ManyToMany(mappedBy = "categories") val items: Set<BucketListItem>? = null,
-        @ManyToOne val parent: Category? = null,
+        @ManyToOne @JoinColumn(name = "parent_id") val parent: Category? = null,
         @OneToMany(cascade = [CascadeType.ALL], mappedBy = "parent")
         val subcategories: Set<Category>? = null,
-        @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_generator")
+        @SequenceGenerator(name = "category_generator", sequenceName = "category_id_seq")
+        val id: Long? = null,
 )
