df4c503a36de0a4913c2a94cbe74bcfcd6ea6781
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-07-04 10:53:50 +0300

    Map whole bucketlist controller

--- src/main/kotlin/dev/ilkka/bucketlist/BucketListController.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/BucketListController.kt
@@ -6,20 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/bucketlist")
 class BucketListController {
     @Autowired private lateinit var bucketListItemRepository: BucketListItemRepository
 
     // Return a list of bucket list items
-    @GetMapping("/bucketlist")
+    @GetMapping("/")
     fun getBucketList(): List<BucketListItem> {
         return bucketListItemRepository.findAll().toCollection(destination = ArrayList())
     }
 
     // Add a new bucket list item
-    @PostMapping("/bucketlist")
+    @PostMapping("/")
     fun addBucketListItem(@RequestBody item: BucketListItem): BucketListItem {
         return bucketListItemRepository.save(item)
     }
