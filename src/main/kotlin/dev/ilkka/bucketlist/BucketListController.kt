344e6b916da4d44067f8ae08e24823e72ea52686
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-07-04 11:26:45 +0300

    Implement single item get

--- src/main/kotlin/dev/ilkka/bucketlist/BucketListController.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/BucketListController.kt
@@ -5,6 +5,7 @@ import dev.ilkka.bucketlist.domain.BucketListItemRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -29,5 +30,11 @@ class BucketListController {
     )
     fun addBucketListItem(@RequestBody item: BucketListItem): BucketListItem {
         return bucketListItemRepository.save(item)
+    }
+
+    // Return a single bucket list item
+    @GetMapping("items/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getBucketListItem(@PathVariable id: Long): BucketListItem {
+        return bucketListItemRepository.findById(id).get()
     }
 }
