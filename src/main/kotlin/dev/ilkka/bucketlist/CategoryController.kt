760e0af44e203e0e3496aa87d2f5ee574b4c6419
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-07-04 11:42:24 +0300

    Implement adding category

--- src/main/kotlin/dev/ilkka/bucketlist/CategoryController.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/CategoryController.kt
@@ -1,7 +1,11 @@
 package dev.ilkka.bucketlist
 
+import dev.ilkka.bucketlist.domain.Category
 import dev.ilkka.bucketlist.domain.CategoryRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/category")
 public class CategoryController {
     @Autowired private lateinit var categoryRepository: CategoryRepository
+
+    @PostMapping(
+            consumes = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    fun addCategory(@RequestBody category: Category): Category {
+        return categoryRepository.save(category)
+    }
 }
