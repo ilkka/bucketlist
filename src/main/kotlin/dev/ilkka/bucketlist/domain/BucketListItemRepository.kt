798c82bc8b73e67108b8e77e022fbde3ea144717
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-07-04 10:54:33 +0300

    Use JpaRepository
    CrudRepository comes from Spring Commons and is simpler, JpaRepository
    has pagination and stuff.

--- src/main/kotlin/dev/ilkka/bucketlist/domain/BucketListItemRepository.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/domain/BucketListItemRepository.kt
@@ -1,12 +1,9 @@
 package dev.ilkka.bucketlist.domain
 
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
 
-public interface BucketListItemRepository : CrudRepository<BucketListItem, String> {
-    fun findById(id: Long): BucketListItem
-    fun findByTitle(title: String): BucketListItem
-
+public interface BucketListItemRepository : JpaRepository<BucketListItem, Long> {
     @Query("SELECT b FROM BucketListItem b JOIN b.categories c WHERE c.title = ?1")
     fun findByCategoryName(category: String): List<BucketListItem>
 }
