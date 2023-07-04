d54bee2818ee5c2c56cd83e25ac68ed840e11fca
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-07-04 11:26:03 +0300

    Stop gradle complaining about implicit test deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.testcontainers:postgresql")
 }
 
