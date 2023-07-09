4b56468eaf31234e5670b57bbf22c1c1d759a6c6
Ilkka Poutanen <ilkka.poutanen@futurice.com>
2023-07-09 17:48:30 +0300

    Helper class for incoming items
    The categories can be just IDs here

--- src/main/kotlin/dev/ilkka/bucketlist/BucketListController.kt
+++ src/main/kotlin/dev/ilkka/bucketlist/BucketListController.kt
@@ -2,6 +2,7 @@ package dev.ilkka.bucketlist
 
 import dev.ilkka.bucketlist.domain.BucketListItem
 import dev.ilkka.bucketlist.domain.BucketListItemRepository
+import dev.ilkka.bucketlist.domain.CategoryRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,10 +12,17 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+class NewBucketListItem(
+        val title: String,
+        val description: String,
+        val categories: Set<Long>? = null
+)
+
 @RestController
 @RequestMapping("/bucketlist")
 class BucketListController {
     @Autowired private lateinit var bucketListItemRepository: BucketListItemRepository
+    @Autowired private lateinit var categoryRepository: CategoryRepository
 
     // Return a list of bucket list items
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
@@ -27,8 +35,17 @@ class BucketListController {
             consumes = [MediaType.APPLICATION_JSON_VALUE],
             produces = [MediaType.APPLICATION_JSON_VALUE]
     )
-    fun addBucketListItem(@RequestBody item: BucketListItem): BucketListItem {
-        return bucketListItemRepository.save(item)
+    fun addBucketListItem(@RequestBody item: NewBucketListItem): BucketListItem {
+        // Get all the categories in the item
+        val categories = item.categories?.map { id -> categoryRepository.findById(id).get() }
+        // create a new bucket list item with the categories and the values from the request body
+        val newItem =
+                BucketListItem(
+                        title = item.title,
+                        description = item.description,
+                        categories = categories?.toSet()
+                )
+        return bucketListItemRepository.save(newItem)
     }
 
     // Return a single bucket list item
